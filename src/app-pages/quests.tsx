0653f401a1c6fff50aea5196b385175e01bd960c
kirillweb2023 <webrazrabotka72@gmail.com>
2025-10-14 15:54:09 +0300

    Update kirill

--- src/app-pages/quests.tsx
+++ src/app-pages/quests.tsx
@@ -30,32 +30,34 @@ export function QuestPage({ quest }: QuestPageProps) {
 
     return (
         <div className="quest">
-            <QuestNavigate questName={quest.nameQuest} />
-            <div className="quest__wrapper">
-                <div className="quest__info">
-                    <QuestTitle title={quest.nameQuest} />
-                    <QuestBlock
-                        location={quest.locationQuest}
-                        price={quest.priceQuest}
-                        maxPeople={quest.maxPeople}
-                        level={quest.level}
-                    />
-                    <QuestText description={quest.descrQuest} />
+            <div className="container">
+                <QuestNavigate questName={quest.nameQuest} />
+                <div className="quest__wrapper">
+                    <div className="quest__info">
+                        <QuestTitle title={quest.nameQuest} />
+                        <QuestBlock
+                            location={quest.locationQuest}
+                            price={quest.priceQuest}
+                            maxPeople={quest.maxPeople}
+                            level={quest.level}
+                        />
+                        <QuestText description={quest.descrQuest} />
+                    </div>
+
+                    <QuestImages />
                 </div>
 
-                <QuestImages />
-            </div>
+                <Record onTimeSelect={handleTimeSelect} />
 
-            <Record onTimeSelect={handleTimeSelect} />
-            
-            {showBooking && selectedTime && (
-                <Booking 
-                    date={selectedTime.date}
-                    time={selectedTime.time}
-                    onClose={handleCloseBooking}
-                    questPrice={quest.priceQuest}
-                />
-            )}
+                {showBooking && selectedTime && (
+                    <Booking
+                        date={selectedTime.date}
+                        time={selectedTime.time}
+                        onClose={handleCloseBooking}
+                        questPrice={quest.priceQuest}
+                    />
+                )}
+            </div>
         </div>
     );
 }
